cbf236b93492d494c97e67cef7debdd3c52736dc
Cedric Nixon <cnixon@berkeley.edu>
2017-04-11 16:47:43 -0700

    Finished

--- SnapchatClonePt3/PostsTableViewController.swift
+++ SnapchatClonePt3/PostsTableViewController.swift
@@ -52,6 +52,7 @@ class PostsTableViewController: UIViewController, UITableViewDelegate, UITableVi
     */
     override func viewWillAppear(_ animated: Bool) {
         // YOUR CODE HERE
+        updateData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,6 +73,19 @@ class PostsTableViewController: UIViewController, UITableViewDelegate, UITableVi
     */
     func updateData() {
         // YOUR CODE HERE
+        getPosts(user: currentUser, completion: {(posts) in
+            if posts != nil {
+                clearThreads()
+                for p in posts! {
+                    addPostToThread(post: p)
+                    getDataFromPath(path: p.postImagePath, completion: {(d) in
+                        self.loadedImagesById[p.postId] = UIImage(data: d!)
+                    })
+                }
+                self.postTableView.reloadData()
+            }
+        })
+        
     }
     
     // MARK: Custom methods (relating to UI)
@@ -145,6 +159,7 @@ class PostsTableViewController: UIViewController, UITableViewDelegate, UITableVi
             post.read = true
             
             // YOUR CODE HERE
+            currentUser.addNewReadPost(postID: post.postId)
             
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }
